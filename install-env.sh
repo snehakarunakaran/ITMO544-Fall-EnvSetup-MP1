bc5e678ff7f47b929dfb654923ad86889295acaa
snehakarunakaran <skaruna1@hawk.iit.edu>
2015-10-27 20:20:01 -0500

    checking

--- install-env.sh
+++ install-env.sh
@@ -6,11 +6,14 @@ sudo curl -sS https://getcomposer.org/installer | php
 sudo php composer.phar require aws/aws-sdk-php
 
 
-sudo git clone https://github.com/snehakarunakaran/ITMO544-Fall-Application.git
+sudo git clone https://github.com/snehakarunakaran/ITMO544-Fall-Application-MP1.git
 
-mv ./ITMO544-Fall-Application/images /var/www/html/images
-mv ./ITMO544-Fall-Application/index.html /var/www/html
-mv ./ITMO544-Fall-Application/page2.html /var/www/html
+sudo mv ITMO544-Fall-Application-MP1 /var/www/html
+sudo mv /var/www/html/ITMO544-Fall-Application-MP1/*.php /var/www/html
+pw=letmein
+export pw
+
+
 
 echo "Success" > /tmp/hello.txt
 
