0f7e1c5f48c006567304caf6e3031421a2790fc4
snehakarunakaran <skaruna1@hawk.iit.edu>
2015-11-09 03:28:57 -0600

    minor changes

--- install-env.sh
+++ install-env.sh
@@ -5,11 +5,11 @@ sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 git clone https://github.com/snehakarunakaran/ITMO544-Fall-Application-MP1.git
 
 #mv ./ITMO544-Fall-Application-MP1/images /var/www/html/images
-mv ./ITMO544-Fall-Application-MP1/index.html /var/www/html
-mv ./ITMO544-Fall-Application-MP1/*.php /var/www/html
-mv ./ITM0544-Fall-Application-MP1/*.png /var/www/html
-mv ./ITM0544-Fall-Application-MP1/*.js /var/www/html
-mv ./ITM0544-Fall-Application-MP1/*.css /var/www/html
+sudo mv ./ITMO544-Fall-Application-MP1/index.html /var/www/html
+sudo mv ./ITMO544-Fall-Application-MP1/*.php /var/www/html
+sudo mv ./ITM0544-Fall-Application-MP1/*.png /var/www/html
+sudo mv ./ITM0544-Fall-Application-MP1/*.js /var/www/html
+sudo mv ./ITM0544-Fall-Application-MP1/*.css /var/www/html
 
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
@@ -17,7 +17,7 @@ sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 
 sudo mv vendor /var/www/html &> /tmp/movevendor.txt
 
-#sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
 
 echo "Hello!" > /tmp/hello.txt
 
