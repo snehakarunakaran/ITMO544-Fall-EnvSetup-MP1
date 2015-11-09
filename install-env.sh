10f82d4bf629c2bd4f180a8b3727078da8775eda
snehakarunakaran <skaruna1@hawk.iit.edu>
2015-11-09 04:48:38 -0600

    final push

--- install-env.sh
+++ install-env.sh
@@ -4,13 +4,11 @@ sudo apt-get update -y
 sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 git clone https://github.com/snehakarunakaran/ITMO544-Fall-Application-MP1.git
 
-#sudo mv ./ITMO544-Fall-Application-MP1/images /var/www/html/images
 sudo mv ./ITMO544-Fall-Application-MP1/index.html /var/www/html
+sudo mv ./ITMO544-Fall-Application-MP1/*.css /var/www/html
+sudo mv ./ITMO544-Fall-Application-MP1/*.js /var/www/html
+sudo mv ./ITMO544-Fall-Application-MP1/*.png /var/www/html
 sudo mv ./ITMO544-Fall-Application-MP1/*.php /var/www/html
-sudo mv ./ITM0544-Fall-Application-MP1/fotorama.png /var/www/html
-sudo mv ./ITM0544-Fall-Application-MP1/fotorama@2x.png /var/www/html
-sudo mv ./ITM0544-Fall-Application-MP1/fotorama.js /var/www/html
-sudo mv ./ITM0544-Fall-Application-MP1/fotorama.css /var/www/html
 
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
